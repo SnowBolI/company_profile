3f391afa6f2ba8cdd2d10d2844aba6a69b7e5f3b
bagasdwii <gasdwi1908@gmail.com>
2024-10-24 13:44:21 +0700

    CRUD SEJARAH VISI DAN LOAD SEJARAH

---
+++ resources/views/profileBanners/edit.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.admin')
+
+@section('title', 'Edit Profile Banner')
+
+@section('breadcrumbs', 'Profile Banner')
+
+@section('second-breadcrumb')
+    <li>Edit</li>
+@endsection
+
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <a href="/admin/profile_banner" class="mb-3 btn btn-primary">Kembali</a>
+                    <form action="{{ route('profile_banner.update', $profileBanner->id) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-3">
+                            <label for="judul" class="font-weight-bold">Judul</label>
+                            <input type="text" id="judul" class="form-control {{ $errors->first('judul') ? 'is-invalid' : '' }}" name="judul" placeholder="Judul" value="{{ old('judul', $profileBanner->judul) }}" required>
+                            <div class="invalid-feedback">{{ $errors->first('judul') }}</div>
+                        </div>
+                        {{-- <div class="mb-3">
+                            <label for="keterangan" class="font-weight-bold">Keterangan</label>
+                            <textarea id="keterangan" class="form-control {{ $errors->first('keterangan') ? 'is-invalid' : '' }}" name="keterangan" placeholder="Keterangan" required>{{ old('keterangan', $profileBanner->keterangan) }}</textarea>
+                            <div class="invalid-feedback">{{ $errors->first('keterangan') }}</div>
+                        </div> --}}
+                        <div class="mb-3">
+                            <label for="gambar" class="font-weight-bold">Gambar</label>
+                            @if($profileBanner->gambar)
+                                <img class="img-fluid rounded mx-auto d-block my-3" src="{{ Storage::url($profileBanner->gambar) }}" alt="Gambar Produk">
+                            @else
+                                <p>Tidak ada gambar yang diunggah.</p>
+                            @endif
+                            <input type="file" id="gambar" class="form-control {{ $errors->first('gambar') ? 'is-invalid' : '' }}" name="gambar">
+                            <div class="invalid-feedback">{{ $errors->first('gambar') }}</div>
+                        </div>
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-success">Simpan</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
