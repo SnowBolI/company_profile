26313fb5cbee56e853633e1c64696f64f4fcef6f
Adhity <64183277+SnowBolI@users.noreply.github.com>
2024-11-13 10:34:01 +0700

    update ui kontak (#58)
    Co-authored-by: AYAYA\User <inmydream375@gmail.com>

--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -38,77 +38,75 @@
 
 @section('content')
 
-  <section id="contact">
-    <div class="container wow fadeInUp">
-      <div class="section-header">
-        <h3 class="section-title">Contact</h3>
-        <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
-      </div>
-    </div>
-
-    {{-- <div id="google-map" data-latitude="40.713732" data-longitude="-74.0092704"></div> --}}
-
-    <div class="container wow fadeInUp">
-      <div class="row justify-content-center">
-
-        <div class="col-lg-3 col-md-4">
-
-          <div class="info">
-            <div>
-              <i class="fa fa-map-marker"></i>
-              <p>Gedong Tengen,
-                <br>Daerah Istimewa Yogyakarta</p>
-            </div>
-
-            <div>
-              <i class="fa fa-envelope"></i>
-              <p>danyadhi4149@gmail.com</p>
-            </div>
-
-            <div>
-              <i class="fa fa-phone"></i>
-              <p>0831-6179-3990</p>
-            </div>
-          </div>
-
-          <div class="social-links">
-            <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-            <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-            <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
-            <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
-            <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
-          </div>
-
+<section id="contact">
+    <div class="container">
+        <div class="section-header">
+            <h3 class="section-title">HUBUNGI KAMI</h3>
+            <p class="section-description">Kami Siap Melayani Anda Sepenuh Hati</p>
         </div>
 
-        <div class="col-lg-5 col-md-8">
-          <div class="form">
-            <div id="sendmessage">Your message has been sent. Thank you!</div>
-            <div id="errormessage"></div>
-            <form action="" method="post" role="form" class="contactForm">
-              <div class="form-group">
-                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                <div class="validation"></div>
-              </div>
-              <div class="form-group">
-                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                <div class="validation"></div>
-              </div>
-              <div class="form-group">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                <div class="validation"></div>
-              </div>
-              <div class="form-group">
-                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                <div class="validation"></div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
-          </div>
+        <!-- Contact Info Section -->
+        <div class="contact-info-section">
+            <div class="info">
+            <div class="info-address">
+    <i class="fa fa-map-marker" style="font-size:24px; color:#2dc997;"></i>
+    <p>
+      Jl. Raya Jaranan
+      Ngadirejo, Kecamatan Kawedanan, Kabupaten Magetan
+      Provinsi Jawa Timur
+      63382
+    </p>
+                </div>
+
+                <div>
+                    <i class="fas fa-envelope"></i>
+                    <p>bank.ekadharma@gmail.com</p>
+                </div>
+
+                <div>
+                    <i class="fas fa-phone"></i>
+                    <p>(0351) 439872</p>
+                </div>
+            </div>
+
+            <div class="social-links">
+                <a href="#" class="twitter"><i class="fab fa-twitter"></i></a>
+                <a href="#" class="facebook"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="instagram"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="linkedin"><i class="fab fa-linkedin"></i></a>
+            </div>
         </div>
 
-      </div>
+        <!-- Map and Form Container -->
+        <div class="map-form-container">
+            <!-- Map Section -->
+            <div class="map-section">
+            <iframe 
+                src="https://www.google.com/maps?q=Bank%20Ekadharma,%20Jl.%20Raya%20Jaranan%20%E2%80%93%20Ngadirejo,%20Kecamatan%20Kawedanan,%20Kabupaten%20Magetan,%20Jawa%20Timur%2063382&output=embed" 
+                width="100%" 
+                height="450" 
+                style="border:0;" 
+                allowfullscreen="" 
+                loading="lazy" 
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+            </div>
 
+            <!-- Form Section -->
+            <div class="form-section">
+                <div class="form">
+                    <form action="" method="post" role="form" class="contactForm">
+                        <input type="text" name="name" placeholder="Your Name" required>
+                        <input type="email" name="email" placeholder="Your Email" required>
+                        <input type="text" name="subject" placeholder="Subject" required>
+                        <textarea name="message" placeholder="Message" required></textarea>
+                        <div style="text-align: center;">
+                            <button type="submit">Send Message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-  </section><!-- #contact -->
-@endsection
+</section>
+    @endsection
