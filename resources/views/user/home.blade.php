c9a41c0c90aa8343374ad639add7b918b04b4e6c
Adhity <64183277+SnowBolI@users.noreply.github.com>
2024-11-18 08:37:48 +0700

    fix slider home (#69)
    Co-authored-by: AYAYA\User <inmydream375@gmail.com>

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -45,29 +45,26 @@
     </style>    
 @endsection
 
-@section('hero')
 <section id="hero">
-  <div class="hero-container">
-<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
     <div class="carousel-inner">
-        @foreach ($homeSliders as $index => $slider)
-            <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
-                <img src="{{ Storage::url($slider->gambar) }}" class="d-block w-100" alt="Slide {{ $index + 1 }}">
-            </div>
-        @endforeach
+      @foreach ($homeSliders as $index => $slider)
+        <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
+          <img src="{{ Storage::url($slider->gambar) }}" class="d-block w-100" alt="Slide {{ $index + 1 }}">
+        </div>
+      @endforeach
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
     </button>
     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
     </button>
-</div>
   </div>
 </section>
-@endsection
+
 
 {{-- @section('hero')
 <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
